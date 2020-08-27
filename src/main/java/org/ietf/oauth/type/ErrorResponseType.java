92adf5fbfb724b2b0c34195d9bb77de4de7c1bf3
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2020-08-27 09:16:18 -0400

    add missing entries, add javadoc

--- src/main/java/org/ietf/oauth/type/ErrorResponseType.java
+++ src/main/java/org/ietf/oauth/type/ErrorResponseType.java
@@ -16,7 +16,7 @@
 package org.ietf.oauth.type;
 
 /**
- * RFC 6749 OAuth 2.0 4.1.2.1. Error Response
+ * RFC 6749 OAuth 2.0 5.2. Error Response and OpenId 4.1.2.1.
  * <p>
  * If the request fails due to a missing, invalid, or mismatching redirection
  * URI, or if the client identifier is missing or invalid, the authorization
@@ -28,15 +28,27 @@ package org.ietf.oauth.type;
  */
 public enum ErrorResponseType {
 
-  // OAUTH codes
   /**
-   * The request is missing a required parameter, includes an invalid parameter
-   * value, includes a parameter more than once, or is otherwise malformed.
+   * The request is missing a required parameter, includes an unsupported
+   * parameter value (other than grant type), repeats a parameter, includes
+   * multiple credentials, utilizes more than one mechanism for authenticating
+   * the client, or is otherwise malformed.
    */
   invalid_request,
   /**
-   * The client is not authorized to request an authorization code using this
-   * method.
+   * Client authentication failed (e.g., unknown client, no client
+   * authentication included, or unsupported authentication method). The
+   * authorization server MAY return an HTTP 401 (Unauthorized) status code to
+   * indicate which HTTP authentication schemes are supported. If the client
+   * attempted to authenticate via the "Authorization" request header field, the
+   * authorization server MUST respond with an HTTP 401 (Unauthorized) status
+   * code and include the "WWW-Authenticate" response header field matching the
+   * authentication scheme used by the client.
+   */
+  invalid_client,
+  /**
+   * The authenticated client is not authorized to use this authorization grant
+   * type.
    */
   unauthorized_client,
   /**
@@ -44,10 +56,21 @@ public enum ErrorResponseType {
    */
   access_denied,
   /**
+   * The authorization grant type is not supported by the authorization server.
+   */
+  unsupported_grant_type,
+  /**
    * The authorization server does not support obtaining an authorization code
    * using this method.
    */
   unsupported_response_type,
+  /**
+   * The provided authorization grant (e.g., authorization code, resource owner
+   * credentials) or refresh token is invalid, expired, revoked, does not match
+   * the redirection URI used in the authorization request, or was issued to
+   * another client.
+   */
+  invalid_grant,
   /**
    * The requested scope is invalid, unknown, or malformed.
    */
