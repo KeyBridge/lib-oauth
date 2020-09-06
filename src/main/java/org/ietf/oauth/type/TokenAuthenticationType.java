cd29ccf3c789c6e476646ab6fa1beecacdfa9ae1
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2020-09-06 19:57:09 -0400

    add OpenId Connect types

--- src/main/java/org/ietf/oauth/type/TokenAuthenticationType.java
+++ src/main/java/org/ietf/oauth/type/TokenAuthenticationType.java
@@ -22,10 +22,11 @@ package org.ietf.oauth.type;
  * String indicator of the requested authentication method for the token
  * endpoint.
  *
- * @author Key Bridge
- * @since v2.1.0 created 2020-08-27
  * @see <a href="https://tools.ietf.org/html/rfc7591">OAuth 2.0 Dynamic Client
  * Registration Protocol</a>
+ * @author Key Bridge
+ * @since v2.1.0 created 2020-08-27
+ * @since v2.2.0 add OpenId Connect types 2020-09-06
  */
 public enum TokenAuthenticationType {
 
@@ -39,12 +40,10 @@ public enum TokenAuthenticationType {
    * to a user's resources and access is tied to a user's active session at the
    * authorization server.
    * <p>
-   * Public applications typically are assigned the following grant_types
-   * enabled:
-   * <p>
-   * - implicit <br>
-   * - authorization_code <br>
-   * - refresh_token
+   * The Client does not authenticate itself at the Token Endpoint, either
+   * because it uses only the Implicit Flow (and so does not use the Token
+   * Endpoint) or because it is a Public Client with no Client Secret or other
+   * authentication mechanism.
    */
   none,
   /**
@@ -63,5 +62,77 @@ public enum TokenAuthenticationType {
    * authentication scheme as defined in [RFC2617] to authenticate with the
    * authorization server.
    */
-  client_secret_basic
+  client_secret_basic,
+
+  //  OpenID Connect Core 1.0 Section 9 Client Authentication
+  /**
+   * Clients that have received a client_secret value from the Authorization
+   * Server create a JWT using an HMAC SHA algorithm, such as HMAC SHA-256. The
+   * HMAC (Hash-based Message Authentication Code) is calculated using the
+   * octets of the UTF-8 representation of the client_secret as the shared key.
+   * <p>
+   * The JWT MUST contain the following REQUIRED Claim Values and MAY contain
+   * the following OPTIONAL Claim Values:
+   * <p>
+   * iss - REQUIRED. Issuer. This MUST contain the client_id of the OAuth
+   * Client.
+   * <br>
+   * sub - REQUIRED. Subject. This MUST contain the client_id of the OAuth
+   * Client.
+   * <br>
+   * aud - REQUIRED. Audience. The aud (audience) Claim. Value that identifies
+   * the Authorization Server as an intended audience. The Authorization Server
+   * MUST verify that it is an intended audience for the token. The Audience
+   * SHOULD be the URL of the Authorization Server's Token Endpoint.
+   * <br>
+   * jti - REQUIRED. JWT ID. A unique identifier for the token, which can be
+   * used to prevent reuse of the token. These tokens MUST only be used once,
+   * unless conditions for reuse were negotiated between the parties; any such
+   * negotiation is beyond the scope of this specification.
+   * <br>
+   * exp - REQUIRED. Expiration time on or after which the ID Token MUST NOT be
+   * accepted for processing.
+   * <br>
+   * iat - OPTIONAL. Time at which the JWT was issued.
+   * <p>
+   * The authentication token MUST be sent as the value of the
+   * [OAuth.Assertions] client_assertion parameter. The value of the
+   * [OAuth.Assertions] client_assertion_type parameter MUST be
+   * "urn:ietf:params:oauth:client-assertion-type:jwt-bearer", per [OAuth.JWT].
+   */
+  client_secret_jwt,
+  /**
+   * Clients that have registered a public key sign a JWT using that key. The
+   * JWT MUST contain the following REQUIRED Claim Values and MAY contain the
+   * following OPTIONAL Claim Values:
+   * <p>
+   * iss - REQUIRED. Issuer. This MUST contain the client_id of the OAuth
+   * Client.
+   * <br>
+   * sub - REQUIRED. Subject. This MUST contain the client_id of the OAuth
+   * Client.
+   * <br>
+   * aud - REQUIRED. Audience. The aud (audience) Claim. Value that identifies
+   * the Authorization Server as an intended audience. The Authorization Server
+   * MUST verify that it is an intended audience for the token. The Audience
+   * SHOULD be the URL of the Authorization Server's Token Endpoint.
+   * <br>
+   * jti - REQUIRED. JWT ID. A unique identifier for the token, which can be
+   * used to prevent reuse of the token. These tokens MUST only be used once,
+   * unless conditions for reuse were negotiated between the parties; any such
+   * negotiation is beyond the scope of this specification.
+   * <br>
+   * exp - REQUIRED. Expiration time on or after which the ID Token MUST NOT be
+   * accepted for processing.
+   * <br>
+   * iat - OPTIONAL. Time at which the JWT was issued.
+   * <br>
+   * The JWT MAY contain other Claims. Any Claims used that are not understood
+   * MUST be ignored. The authentication token MUST be sent as the value of the
+   * [OAuth.Assertions] client_assertion parameter. The value of the
+   * [OAuth.Assertions] client_assertion_type parameter MUST be
+   * "urn:ietf:params:oauth:client-assertion-type:jwt-bearer", per [OAuth.JWT].
+   */
+  private_key_jwt,
+
 }
